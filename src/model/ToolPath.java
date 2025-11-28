6077f6900661dcd3a6d16e76c8f3e3252de98bb0
test <test@test-Latitude-E5470>
2025-11-28 15:40:53 +0100

    Add licence header to some files

--- src/model/ToolPath.java
+++ src/model/ToolPath.java
@@ -1,9 +1,31 @@
+/*********************************************************************\
+ * TableViewDummyModelChangeListener.java - xmlCam G-Code Generator  *
+ * Copyright (C) 2025, Christian Kirsch                              *
+ *                                                                   *
+ * This program is free software; you can redistribute it and/or     *
+ * modify it under the terms of the GNU General Public License as    *
+ * published by the Free Software Foundation; either version 3 of    *
+ * the License, or (at your option) any later version.               *
+ *                                                                   *
+ * This program is distributed in the hope that it will be useful,   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of    *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     *
+ * GNU General Public License for more details.                      *
+ *                                                                   *
+ * You should have received a copy of the GNU General Public License *
+ * along with this program; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.         *
+\*********************************************************************/
+
 package model;
 
 import java.awt.geom.Point2D;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
+/**
+ * This class represents a ToolPath which consists an list of points with x and y coordinates.
+ */
 public class ToolPath extends ArrayList<Point2D.Double> {
 	
 	private static final long serialVersionUID = 1L;
@@ -79,14 +101,6 @@ public class ToolPath extends ArrayList<Point2D.Double> {
 	 */
 	public boolean isPocket() {
 		return pocket;
-	}
-
-	/**
-	 * Sets if it is a pocket or not.
-	 * @param pocket The boolean value
-	 */
-	protected void setPocket(boolean pocket) {
-		this.pocket = pocket;
 	}
 	
 }
